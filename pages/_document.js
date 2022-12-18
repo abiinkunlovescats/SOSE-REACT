3f0d55d3ca2a1b2f3d24db8dfef0397305968d6b
abiinkunlovescats <pabhinn771@outlook.com>
2022-12-18 20:14:57 +0530

    Initial WORK

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import { useState } from 'react'
+import Header from '../components/Header'
+import AccountForm from '../components/AccountForm'
+import Footer from '../components/Footer'
 export default function Document() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <Header />
+        <body>
         <Main />
+        <Account isOpen={isOpen}/>
         <NextScript />
       </body>
+      <Footer/>
     </Html>
   )
+}
+function Account(isOpen){
+  if(isOpen === true ){
+  return(<AccountForm/>)
+}
 }
