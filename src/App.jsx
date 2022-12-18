8028b550ae8dc9e699ac8d1429697dd5d3fc12b2
abiinkunlovescats <pabhinn771@outlook.com>
2022-12-18 17:21:16 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+import Home from "./Home.jsx";
+import React, { useState } from "react";
+import About from "./About";
+import { Courses } from "./Courses.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AccountForm from "./components/AccountForm.jsx";
+function App() {
+    
+  return (
+    <React.Fragment>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="about" element={<About />} />
+          <Route exact path="courses" element={<Courses />} />
+        </Routes>
 
-import Home from './Home.jsx'
-import React, { useEffect } from 'react'
-
- function App() {
-
-    return (
-        <React.Fragment>
-            <Home />
-        </React.Fragment>
-    )
+        <Footer />
+      </Router>
+    </React.Fragment>
+  );
 }
 
-export default App
-
-
-
-
+export default App;
